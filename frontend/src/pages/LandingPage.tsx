68ea0a970f80b4473d7d27c1579b3a3a9c6612b8
Davooood90 <davooood90@gmail.com>
2025-02-01 14:19:20 -0800

    Some stuff to do with landing page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import Title from "./sections/Title";
+import Schedule from "./sections/Schedule";
+import About from "./sections/About";
+import Judges from "./sections/Judges";
+import FAQ from "./sections/FAQ";
+import Sponsors from "./sections/Sponsors";
+
+import "./LandingPage.css";
 
 export default function LandingPage() {
   return (
-    <h1>hello</h1>
+    <div classname="landing-page">
+      <Title />
+      <About />
+      <Schedule />
+      <Judges />
+      <FAQ />
+      <Sponsors />
+    </div>
   );
 }
-
-
-
-
---
+++ frontend/src/pages/sections/Title.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Title.css";
+import "../LandingPage.css";
+
+export default function Title() {
+  return (
+    <div className="title-gradient">
+      <div className="title-background">
+        <div className="title-content">
+          <h1>Bootcamp</h1>
+          <h2>Pre-Fellowship Workshop</h2>
+          <p>Learn the skills you need to succeed in the MLH Fellowship</p>
+        </div>
+      </div>
+    </div>
+  );
+}
