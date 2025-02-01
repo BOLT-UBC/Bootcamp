71e58c820a73005cd825f08a355bdb27fe45e3f4
Andrew Ahn <andrewahn21@gmail.com>
2025-02-01 14:15:48 -0800

    Set up pages/routing & bits of skeleton code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import StartRegistration from './pages/StartRegistration';
+import UserInfo from './pages/UserInfo';
+import Responses from './pages/Responses';
+import Team from './pages/Team';
+import ThankYou from './pages/ThankYou';
 
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="/registration" element={<StartRegistration />} />
+                <Route path="/registration/userInfo" element={<UserInfo />} />
+                <Route path="/registration/responses" element={<Responses />} />
+                <Route path="/registration/team" element={<Team />} />
+                <Route path="/registration/thankyou" element={<ThankYou />} />
             </Routes>
         </Router>
     )
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 export default function LandingPage() {
+  const navigate = useNavigate();
+
   return (
-    <h1>hello</h1>
+    <>
+      <h1>Booot camp stuff here</h1>
+      <button onClick={() => navigate("/registration")}>
+        Go to register rn PLSSSSSS
+      </button>
+    </>
   );
 }
 
