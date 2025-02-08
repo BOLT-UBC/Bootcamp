7550bbe203a3600dc519f6e29a067c5a031e8988
Davooood90 <davooood90@gmail.com>
2025-02-07 20:05:13 -0800

    imported new font

--- frontend/src/pages/sections/About.tsx
+++ frontend/src/pages/sections/About.tsx
@@ -3,5 +3,9 @@ import "./About.css";
 import "../LandingPage.css";
 
 export default function About() {
-  return <div className="about-background"></div>;
+  return (
+    <div className="about-background">
+      <div></div>
+    </div>
+  );
 }
