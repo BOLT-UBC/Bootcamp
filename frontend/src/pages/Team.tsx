9021b5373d6cb18b8372c2b1694acd5aaa7af809
Leo <leoshang12@gmail.com>
2025-02-07 21:30:01 -0800

    Folder Right Done

--- frontend/src/pages/Team.tsx
+++ frontend/src/pages/Team.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import FolderRight from "../components/FolderRight";
+import "./Team.css"
 
 export default function Team() {
   const navigate = useNavigate();
   return (
-    <>
-    <h1>Make ur team</h1>
-      <button onClick={() => navigate("/registration/thankyou")}>
-        Next
-      </button>
-    </>
+    <FolderRight>
+      <div className="team__content-wrapper">
+        <p>You are not in a team yet</p>
+        <div className="team__buttons-wrapper">
+          <button>Join Team</button>
+          <button>Create Team</button>
+        </div>
+      </div>
+    </FolderRight>
   );
 }
-
-
-
-
