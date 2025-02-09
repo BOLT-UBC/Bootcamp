d07c904b7a8e477d1b02f45f2632790223c15d7c
Leo <leoshang12@gmail.com>
2025-02-08 16:20:47 -0800

    Full folder pushed

--- frontend/src/pages/Team.tsx
+++ frontend/src/pages/Team.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import FolderWide from "../components/FolderWide";
-import FolderThin from "../components/FolderThin";
+import FullFolder from "../components/Folder/FullFolder";
 import "./Team.css";
 
 export default function Team() {
   const navigate = useNavigate();
-  return <FolderThin></FolderThin>;
+  return <div className="hehehaha">
+
+    <FullFolder></FullFolder>
+  </div>
+  
 }
