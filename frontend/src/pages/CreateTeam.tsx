960124271e5d504e5319798b1131bec3e2a7fd6f
Andrew Ahn <andrewahn21@gmail.com>
2025-02-10 01:39:21 -0800

    feat: team edge cases + styling

--- frontend/src/pages/CreateTeam.tsx
+++ frontend/src/pages/CreateTeam.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import ShortText from '../components/ShortText';
 import { supabase } from '../supabase';
+import "./Dashboard.css";
+import FullFolder from "../components/Folder/FullFolder";
 
 export default function CreateTeam() {
   const navigate = useNavigate();
@@ -9,6 +11,18 @@ export default function CreateTeam() {
   const [shortId, setId] = useState<string>("");
   const [madeTeam, setMadeTeam] = useState<boolean>(false);
   const [message, setMessage] = useState<string>("");
+  const [activePage, setActivePage] = useState("Dashboard");
+
+  const signOut = async () => {
+      const { error } = await supabase.auth.signOut();
+  
+      if (error) {
+        console.error("Sign Out Error:", error);
+      } else {
+        console.log("User signed out successfully");
+        window.location.href = "/";
+      }
+    };
 
   const generateShortId = (): string => {
     return Math.random().toString(36).substring(2, 6); // Ensures a 4-character string
@@ -51,12 +65,63 @@ export default function CreateTeam() {
     initializeTeamID();
   }, []);
 
+  const teamExists = async () => {
+    const { data, error } = await supabase
+        .from('teams')
+        .select('id')
+        .eq('team_name', teamName);
+    
+    if (error) {
+        return
+    }
+
+    if (data.length !== 0) {
+        return true;
+    } else {
+        return false;
+    }
+ }
+
+    const userHasTeam = async () => {
+        const storedEmail = localStorage.getItem("user_email");
+
+        const { data, error } = await supabase
+            .from('users')
+            .select('team_id')
+            .eq('email', storedEmail);
+
+        if (error) {
+            return
+        }
+
+        if (data[0].team_id !== null) {
+            return true;
+        } else {
+            return false;
+        }
+   }
+
   const send = async () => {
+    let taken = await userHasTeam();
+
+    if (taken) {
+        console.log("You have already joined a team, you cannot create another one.");
+        setMessage("You have already joined a team, you cannot create another one.");
+        return 
+    }
 
     if (teamName.length <= 0) {
         console.log("Team name must not be empty");
         setMessage("Team name must not be empty");
         return
+    }
+
+    let exists = await teamExists();
+
+    if (exists) {
+        console.log("This name is taken.");
+        setMessage("This name is taken.");
+        return 
     }
 
     setMessage("")
@@ -97,30 +162,81 @@ export default function CreateTeam() {
         .eq('email', storedEmail);
 };
 
+const navigationComponent = 
+<div className="portal_page__navbar">
+  <div className="navbar__my_portal_wrapper">
+    <h3 className="navbar__section_title">My Portal</h3>
+    <nav className="navbar__links">
+      <a
+        className="link"
+        onClick={() => setActivePage("Dashboard")}
+        href="#dashboard"
+      >
+        Dashboard
+      </a>
+      <a
+        className="link link__last"
+        onClick={() => setActivePage("My Team")}
+        href="#my-team"
+      >
+        My Team
+      </a>
+    </nav>
+  </div>
+  <div className="navbar__general_wrapper">
+    <h3 className="navbar__section_title">General</h3>
+    <nav className="navbar__links">
+      <a
+        className="link"
+        onClick={() => setActivePage("Schedule")}
+        href="#schedule"
+      >
+        Schedule
+      </a>
+      <a
+        className="link link__last"
+        onClick={() => setActivePage("FAQ")}
+        href="#faq"
+      >
+        FAQ
+      </a>
+    </nav>
+  </div>
+  <button className="logout_button" onClick={() => signOut()}>
+    Log Out{" "}
+  </button>
+</div>
+
 
   return (
     <>
-        <h1>Create a team</h1>
-        <h4>Instructions: do this do that </h4>
-        <div>
-            <ShortText
-                value={teamName} 
-                onChange={(e) => setTeamName(e.target.value)} 
-                placeholder="Create your team name"
-                need={true}
-            />
-            {!madeTeam && <button onClick={send}>
-                Create
-            </button>}
-        
-            {madeTeam && <p>Team {teamName} successfully created! Your team ID is: {shortId}.</p>}
-            {message.length > 0 ? message : null}
+        <div className="hehehaha">
+            <FullFolder portalTitle="Create Team" navbarTitle={"Bootcamp"} navigationComponent={navigationComponent}>
+                <div className="folder-contents">
+                    <h1 className='instruction'>What would you like to name your team?</h1>
+                    <div className="team-input">
+                        <ShortText
+                            value={teamName}
+                            onChange={(e) => setTeamName(e.target.value)}
+                            placeholder="Your Team Name"
+                            need={true} 
+                        />
+                        {!madeTeam && <button className="teams-button-join" onClick={send}>
+                            Create Team
+                        </button>}
+                    </div>
+                    
+                    {madeTeam && <p className='message'>Team {teamName} successfully created! Your team ID is: {shortId}.</p>}
+                    {message.length > 0 ? <p className='message'>{message}</p> : null}
+                    <h4 className='note'>
+                        For someone to join your team, they will need your team ID. Note that we will only show this ID to you once.
+                    </h4>
+                    <button className="teams-button" onClick={() => navigate("/portal/team")}>
+                        Back to Team
+                    </button>
+                </div>
+            </FullFolder>
         </div>
-    
-        <button onClick={() => navigate("/portal/team")}>
-            Back to Team
-        </button>
-
     </>
   );
 }
--- frontend/src/pages/JoinTeam.tsx
+++ frontend/src/pages/JoinTeam.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from "react-router-dom";
 import { supabase } from '../supabase';
 import ShortText from '../components/ShortText';
+import FullFolder from "../components/Folder/FullFolder";
+import "./CreateJoin.css"
 
 export default function JoinTeam() {
   const navigate = useNavigate();
@@ -9,6 +11,7 @@ export default function JoinTeam() {
   const [message, setMessage] = useState<string>("")
   const location = useLocation();
   const [email, setEmail] = useState<string>(location.state?.email || "");
+  const [activePage, setActivePage] = useState("Dashboard");
 
   useEffect(() => {
       const storedEmail = localStorage.getItem("user_email");
@@ -26,6 +29,17 @@ const fetchUserEmail = async () => {
     localStorage.setItem("user_email", data.user.email ?? "");
     }
 };
+
+const signOut = async () => {
+      const { error } = await supabase.auth.signOut();
+  
+      if (error) {
+        console.error("Sign Out Error:", error);
+      } else {
+        console.log("User signed out successfully");
+        window.location.href = "/";
+      }
+    };
 
 
     // gets all teams
@@ -63,10 +77,44 @@ const fetchUserEmail = async () => {
         return count >= 4;
     }
 
+  const teamExists = async () => {
+    const { data, error } = await supabase
+        .from('teams')
+        .select('id')
+        .eq('id', id);
+    
+    if (error) {
+        return
+    }
+
+    if (data.length !== 0) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
+  const userHasTeam = async () => {
+    const { data, error } = await supabase
+        .from('users')
+        .select('team_id')
+        .eq('email', email);
+    
+    if (error) {
+        return
+    }
+    
+    if (data[0].team_id !== null) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
   const join = async () => {
     if (email.length <= 0){
-        console.log("You aren't sign in yet.");
-        setMessage("You aren't sign in yet.");
+        console.log("You aren't signed in yet.");
+        setMessage("You aren't signed in yet.");
         return 
     }
 
@@ -82,6 +130,23 @@ const fetchUserEmail = async () => {
         setMessage("ID has to be length 4.");
         return 
     }
+
+    let exists = await teamExists();
+
+    if (!exists) {
+        console.log("The team with the specified ID does not exist.");
+        setMessage("The team with the specified ID does not exist.");
+        return 
+    }
+
+    let taken = await userHasTeam();
+
+    if (taken) {
+        console.log("You have already joined a team, you cannot join another one.");
+        setMessage("You have already joined a team, you cannot join another one.");
+        return 
+    }
+
     setMessage("")
     let data = await getTeams();
     if (data == null) {
@@ -111,24 +176,80 @@ const fetchUserEmail = async () => {
     }
   }
 
+  const navigationComponent = 
+<div className="portal_page__navbar">
+  <div className="navbar__my_portal_wrapper">
+    <h3 className="navbar__section_title">My Portal</h3>
+    <nav className="navbar__links">
+      <a
+        className="link"
+        onClick={() => setActivePage("Dashboard")}
+        href="#dashboard"
+      >
+        Dashboard
+      </a>
+      <a
+        className="link link__last"
+        onClick={() => setActivePage("My Team")}
+        href="#my-team"
+      >
+        My Team
+      </a>
+    </nav>
+  </div>
+  <div className="navbar__general_wrapper">
+    <h3 className="navbar__section_title">General</h3>
+    <nav className="navbar__links">
+      <a
+        className="link"
+        onClick={() => setActivePage("Schedule")}
+        href="#schedule"
+      >
+        Schedule
+      </a>
+      <a
+        className="link link__last"
+        onClick={() => setActivePage("FAQ")}
+        href="#faq"
+      >
+        FAQ
+      </a>
+    </nav>
+  </div>
+  <button className="logout_button" onClick={() => signOut()}>
+    Log Out{" "}
+  </button>
+</div>
+
   return (
     <>
-        <h1>Join a team</h1>
-        <h4>Enter your team ID below</h4>
-        <ShortText
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-            placeholder="Your Team ID"
-            need={true}
-        />
-        {message.length > 0 ? <p>{message}</p> : null}
-        <h4>Your team ID should have been shown when you created your team.</h4>
-        <button onClick={join}>
-            Join Team
-        </button>
-        <button onClick={() => navigate("/portal/team")}>
-            Back to Team
-        </button>
+        <div className="hehehaha">
+            <FullFolder portalTitle="Join Team" navbarTitle={"Bootcamp"} navigationComponent={navigationComponent}>
+                <div className="folder-contents">
+                    <h1 className='instruction'>Enter your team ID below</h1>
+                    <div className="team-input">
+                        <ShortText
+                            value={id}
+                            onChange={(e) => setId(e.target.value)}
+                            placeholder="Your Team ID"
+                            need={true}
+                        />
+                        <button className="teams-button-join" onClick={join}>
+                            Join Team
+                        </button>
+                    </div>
+                    
+                    {message.length > 0 ? <p className='message'>{message}</p> : null}
+                    <h4 className='note'>
+                        Your team ID should have been provided when your team was created. 
+                        Note that you cannot leave your team once you've joined.
+                    </h4>
+                    <button className="teams-button" onClick={() => navigate("/portal/team")}>
+                        Back to Team
+                    </button>
+                </div>
+            </FullFolder>
+        </div>
     </>
   );
 }
